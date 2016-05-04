fc645ecd145dc3593e06b4a0df1d3d6b21687a02
Tim Macfarlane <timmacfarlane@gmail.com>
2016-05-04 21:50:17 +0200

    allow extend to take undefined arguments

--- extend.js
+++ extend.js
@@ -2,9 +2,11 @@ module.exports = function (object) {
   for(var n = 1; n < arguments.length; n++) {
     var o = arguments[n];
 
-    Object.keys(o).forEach(function (key) {
-      object[key] = o[key];
-    });
+    if (o) {
+      Object.keys(o).forEach(function (key) {
+        object[key] = o[key];
+      });
+    }
   }
 
   return object;
