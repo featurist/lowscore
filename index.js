16cf15f1e5bb00a0af8d818b13cc1fec4cb7d42a
Tim Macfarlane <timmacfarlane@gmail.com>
2016-08-26 17:33:28 +0200

    1.9.0

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ exports.findIndex = require('./findIndex.js');
 exports.flatten = require('./flatten.js');
 exports.groupBy = require('./groupBy.js');
 exports.indexBy = require('./indexBy.js');
+exports.pick = require('./pick.js');
 exports.range = require('./range.js');
 exports.sortBy = require('./sortBy.js');
 exports.times = require('./times.js');
